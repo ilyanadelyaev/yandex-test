bc050b0306943faf3bb6c8df4e970fc242d3d0b2
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-22 12:14:44 +0300

    add find routes logic

--- lib/logic.py
+++ lib/logic.py
@@ -1,12 +1,14 @@
 from django.db import connection
 
-from .models import Station, Direction, DirectionStation
+from . import models
 
 
 def __nodes():
     """
+    fetch all route combinations from graph
+    --
     only near nodes
-    1 and 2: (pos1 = pos2 - 1) or (pos1 = pos2 + 1)
+    st_1 and st_2: (pos1 = pos2 - 1) or (pos1 = pos2 + 1)
     """
     cursor = connection.cursor()
     cursor.execute("""
@@ -20,8 +22,51 @@ def __nodes():
             and
             abs(ds1.position - ds2.position) = 1
     """ % (
-        DirectionStation._meta.db_table,
-        DirectionStation._meta.db_table,
+        models.DirectionStation._meta.db_table,
+        models.DirectionStation._meta.db_table,
+    ))
+    return cursor.fetchall()
+
+
+def __find_routes(start, end, weekday):
+    """
+    find route for selected stations
+    --
+    start station must be lt than end station
+    """
+    cursor = connection.cursor()
+    cursor.execute("""
+        select rs.route_id, tt.time
+        from %s tt left join
+        (
+            select rs1.route_id route_id
+            from %s rs1 inner join %s rs2
+            on
+                rs1.route_id = rs2.route_id
+                and
+                rs1.station_id != rs2.station_id
+                and
+                rs1.position < rs2.position
+            where
+                rs1.station_id = %s
+                and
+                rs2.station_id = %s
+        ) rs
+        on
+            rs.route_id = tt.route_id
+        where
+            rs.route_id is not null
+            and
+            tt.weekday = %s
+        order by
+            tt.time
+    """ % (
+        models.Timetable._meta.db_table,
+        models.RouteStation._meta.db_table,
+        models.RouteStation._meta.db_table,
+        start,
+        end,
+        weekday,
     ))
     return cursor.fetchall()
 
@@ -57,9 +102,9 @@ def search_route(start, end):
     for p in path:
         s, d, e = p
         pp.append((
-            Station.objects.get(pk=s),
-            Direction.objects.get(pk=d),
-            Station.objects.get(pk=e),
+            models.Station.objects.get(pk=s),
+            models.Direction.objects.get(pk=d),
+            models.Station.objects.get(pk=e),
         ))
 
     return pp
