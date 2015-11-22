9b1da71b47939e10bec42c92cff80f1d2a5538c7
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-22 11:13:07 +0300

    update admin view for Route

--- lib/admin.py
+++ lib/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 
-from .models import Station, Direction, DirectionStation, Route
-
-
-class DirectionStationInlite(admin.TabularInline):
-    model = DirectionStation
+from . import models
 
 
 class DirectionAdmin(admin.ModelAdmin):
-    inlines = [DirectionStationInlite]
+    class __DirectionStationInlite(admin.TabularInline):
+        model = models.DirectionStation
+    inlines = [__DirectionStationInlite]
 
 
-admin.site.register(Station)
-admin.site.register(Direction, DirectionAdmin)
-admin.site.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    class __RouteStationsInline(admin.TabularInline):
+        model = models.RouteStation
+    class __TimetableInline(admin.TabularInline):
+        model = models.Timetable
+    inlines = [__RouteStationsInline, __TimetableInline]
+    list_filter = ['direction']
+
+
+admin.site.register(models.Station)
+admin.site.register(models.Direction, DirectionAdmin)
+admin.site.register(models.Route, RouteAdmin)
