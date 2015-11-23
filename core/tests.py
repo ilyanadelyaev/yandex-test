878af6bfe2ef481aaa116ef5b338c9102fb65c9e
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-23 13:28:59 +0300

    add some tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import django.test
 
-# Create your tests here.
+import core.models
+
+
+class StationModelTests(django.test.TestCase):
+    def test__has_fields(self):
+        s = core.models.Station(name='S')
+        s.save()
+        s = core.models.Station.objects.get(pk=s.id)
+        self.assertEqual(s.name, 'S')
+
+    def test__directionstation_list(self):
+        d = core.models.Direction(name='D')
+        d.save()
+        s = core.models.Station(name='S')
+        s.save()
+        self.assertEqual(len(s.directionstation_list()), 0)
+        s.directionstation_set.create(direction=d, position=1)
+        self.assertEqual(len(s.directionstation_list()), 1)
+        ll = set(((i.direction.id, i.direction.name, i.position) for i in s.directionstation_list()))
+        self.assertIn((d.id, d.name, 1), ll)
+
+
+class DirectionModelTests(django.test.TestCase):
+    pass
+
+
+class RouteModelTests(django.test.TestCase):
+    pass
+
+
+class DirectionModelTests(django.test.TestCase):
+    pass
--- view/tests.py
+++ view/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import django.test
 
-# Create your tests here.
+import core.models
+import view.views
+
+
+class DirectionViewTests(django.test.TestCase):
+    def test__DetailView_get_context_data(self):
+        d = core.models.Direction(name='D')
+        v = view.views.DirectionDetailView()
+        v.object = d
+        t = v.get_context_data()
+        self.assertIn('directionstation_list', t)
+        self.assertIn('route_list', t)
