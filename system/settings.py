eacab08195f6c6e805fe4820570c9a3a44f3b32d
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-24 00:15:39 +0300

    drop django model forms view

--- system/settings.py
+++ system/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = (
     'core',
     'api',
     'www',
-    'view',  # debug
 )
 
 MIDDLEWARE_CLASSES = (
--- system/urls.py
+++ system/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     url(r'^', include('www.urls', namespace='www')),
     url(r'^api/', include('api.urls', namespace='api')),
 
-    url(r'^view/', include('view.urls', namespace='view')),  # debug
-
     url(r'^admin/', include(admin.site.urls)),
 ]
