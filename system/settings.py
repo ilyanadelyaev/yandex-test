f9aae1c2b4cdd7eb110a8e63702b0e96e2d76f6f
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-23 16:42:40 +0300

    www: add dummy ajax view pages

--- system/settings.py
+++ system/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'view',
     'api',
+    'www',
+    'view',  # debug
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ www/views.py
@@ -0,0 +1,33 @@
+import django.shortcuts
+
+
+def search(request):
+    return django.shortcuts.render(request, 'www/search.html')
+
+
+def search_results(request):
+    return django.shortcuts.render(request, 'www/search_results.html')
+
+
+def stations_list(request):
+    return django.shortcuts.render(request, 'www/stations_list.html')
+
+
+def station(request, pk):
+    return django.shortcuts.render(request, 'www/station.html', {'pk': pk})
+
+
+def directions_list(request):
+    return django.shortcuts.render(request, 'www/directions_list.html')
+
+
+def direction(request, pk):
+    return django.shortcuts.render(request, 'www/direction.html', {'pk': pk})
+
+
+def routes_list(request):
+    return django.shortcuts.render(request, 'www/routes_list.html')
+
+
+def route(request, pk):
+    return django.shortcuts.render(request, 'www/route.html', {'pk': pk})
