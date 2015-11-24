a104cc8a8448fc9d21314101a3a9cca7b5f0b1db
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-24 20:25:39 +0300

    www: tests: initial

---
+++ trains/www/tests.py
@@ -0,0 +1,57 @@
+import uuid
+
+import django.test
+
+import trains.core.models
+
+
+class ViewTests(django.test.TestCase):
+    def setUp(self):
+        self.client = django.test.Client()
+        #
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
+        s.save()
+        self.station_id = s.id
+        #
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
+        d.save()
+        self.direction_id = d.id
+        #
+        rn = str(uuid.uuid4())
+        r = trains.core.models.Route(name=rn, direction=d, start_station=s, end_station=s)
+        r.save()
+        self.route_id = r.id
+
+    def test__invalid_url(self):
+        resp = self.client.get('/index/')
+        self.assertEqual(resp.status_code, 404)
+
+    def test__search(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test__stations_list(self):
+        resp = self.client.get('/station/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test__station(self):
+        resp = self.client.get('/station/{}/'.format(self.station_id))
+        self.assertEqual(resp.status_code, 200)
+
+    def test__directions_list(self):
+        resp = self.client.get('/direction/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test__direction(self):
+        resp = self.client.get('/direction/{}/'.format(self.direction_id))
+        self.assertEqual(resp.status_code, 200)
+
+    def test__routes_list(self):
+        resp = self.client.get('/route/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test__route(self):
+        resp = self.client.get('/route/{}/'.format(self.route_id))
+        self.assertEqual(resp.status_code, 200)
