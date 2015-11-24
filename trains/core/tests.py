48a96d700e4ec7e75b3f2d18edf3e664262f6f76
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-24 15:01:34 +0300

    core: tests: use uuid for model names

--- trains/core/tests.py
+++ trains/core/tests.py
@@ -1,3 +1,4 @@
+import uuid
 import datetime
 
 import django.test
@@ -7,88 +8,103 @@ import trains.core.models
 
 class StationModelTests(django.test.TestCase):
     def test__has_fields(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
         s = trains.core.models.Station.objects.get(pk=s.id)
-        self.assertEqual(s.name, 'S')
+        self.assertEqual(s.name, sn)
 
     def test__directionstation_list(self):
-        d = trains.core.models.Direction(name='D')
-        d.save()
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
+        d.save()
         self.assertEqual(len(s.directionstation_list()), 0)
         s.directionstation_set.create(direction=d, position=1)
         self.assertEqual(len(s.directionstation_list()), 1)
         ll = set(((i.direction.id, i.direction.name, i.position) for i in s.directionstation_list()))
-        self.assertIn((d.id, d.name, 1), ll)
+        self.assertIn((d.id, dn, 1), ll)
 
 
 class DirectionModelTests(django.test.TestCase):
     def test__has_fields(self):
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
         d = trains.core.models.Direction.objects.get(pk=d.id)
-        self.assertEqual(d.name, 'D')
+        self.assertEqual(d.name, dn)
 
 
 class DirectionStationModelTests(django.test.TestCase):
     def test__direct_creation(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
         ds = trains.core.models.DirectionStation(
             direction=d, station=s, position=4)
         ds.save()
         ds = trains.core.models.DirectionStation.objects.filter(pk=ds.id).first()
-        self.assertEqual(ds.direction.id, d.id)
-        self.assertEqual(ds.station.id, s.id)
+        self.assertEqual(ds.direction.name, dn)
+        self.assertEqual(ds.station.name, sn)
         self.assertEqual(ds.position, 4)
 
     def test__via_direction(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
         ds = d.directionstation_set.create(station=s, position=5)
         ds = trains.core.models.DirectionStation.objects.filter(
             pk=ds.id).first()
-        self.assertEqual(ds.direction.id, d.id)
-        self.assertEqual(ds.station.id, s.id)
+        self.assertEqual(ds.direction.name, dn)
+        self.assertEqual(ds.station.name, sn)
         self.assertEqual(ds.position, 5)
 
     def test__via_station(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
         ds = s.directionstation_set.create(direction=d, position=6)
         ds = trains.core.models.DirectionStation.objects.filter(
             pk=ds.id).first()
-        self.assertEqual(ds.direction.id, d.id)
-        self.assertEqual(ds.station.id, s.id)
+        self.assertEqual(ds.direction.name, dn)
+        self.assertEqual(ds.station.name, sn)
         self.assertEqual(ds.position, 6)
 
 
 class RouteModelTests(django.test.TestCase):
     def test__has_fields(self):
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
-        r = trains.core.models.Route(name='R', direction=d)
+        rn = str(uuid.uuid4())
+        r = trains.core.models.Route(name=rn, direction=d)
         r.save()
         r = trains.core.models.Route.objects.get(pk=r.id)
-        self.assertEqual(r.name, 'R')
-        self.assertEqual(r.direction.name, 'D')
+        self.assertEqual(r.name, rn)
+        self.assertEqual(r.direction.name, dn)
 
 
 class RouteStationModelTests(django.test.TestCase):
     def test__direct_creation(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
-        r = trains.core.models.Route(name='R', direction=d)
+        rn = str(uuid.uuid4())
+        r = trains.core.models.Route(name=rn, direction=d)
         r.save()
         rs = trains.core.models.RouteStation(
             route=r, station=s, position=10,
@@ -97,18 +113,21 @@ class RouteStationModelTests(django.test.TestCase):
         )
         rs.save()
         rs = trains.core.models.RouteStation.objects.filter(pk=rs.id).first()
-        self.assertEqual(rs.route.id, r.id)
-        self.assertEqual(rs.station.id, s.id)
+        self.assertEqual(rs.route.name, rn)
+        self.assertEqual(rs.station.name, sn)
         self.assertEqual(rs.position, 10)
         self.assertEqual(rs.wait_time, datetime.timedelta(1))
         self.assertEqual(rs.move_time, datetime.timedelta(2))
 
     def test__via_route(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
-        r = trains.core.models.Route(name='R', direction=d)
+        rn = str(uuid.uuid4())
+        r = trains.core.models.Route(name=rn, direction=d)
         r.save()
         rs = r.routestation_set.create(
             station=s, position=12,
@@ -116,18 +135,21 @@ class RouteStationModelTests(django.test.TestCase):
             move_time=datetime.timedelta(6),
         )
         rs = trains.core.models.RouteStation.objects.filter(pk=rs.id).first()
-        self.assertEqual(rs.route.id, r.id)
-        self.assertEqual(rs.station.id, s.id)
+        self.assertEqual(rs.route.name, rn)
+        self.assertEqual(rs.station.name, sn)
         self.assertEqual(rs.position, 12)
         self.assertEqual(rs.wait_time, datetime.timedelta(5))
         self.assertEqual(rs.move_time, datetime.timedelta(6))
 
     def test__via_station(self):
-        s = trains.core.models.Station(name='S')
+        sn = str(uuid.uuid4())
+        s = trains.core.models.Station(name=sn)
         s.save()
-        d = trains.core.models.Direction(name='D')
+        dn = str(uuid.uuid4())
+        d = trains.core.models.Direction(name=dn)
         d.save()
-        r = trains.core.models.Route(name='R', direction=d)
+        rn = str(uuid.uuid4())
+        r = trains.core.models.Route(name=rn, direction=d)
         r.save()
         rs = s.routestation_set.create(
             route=r, position=11,
@@ -135,8 +157,8 @@ class RouteStationModelTests(django.test.TestCase):
             move_time=datetime.timedelta(4),
         )
         rs = trains.core.models.RouteStation.objects.filter(pk=rs.id).first()
-        self.assertEqual(rs.route.id, r.id)
-        self.assertEqual(rs.station.id, s.id)
+        self.assertEqual(rs.route.name, rn)
+        self.assertEqual(rs.station.name, sn)
         self.assertEqual(rs.position, 11)
         self.assertEqual(rs.wait_time, datetime.timedelta(3))
         self.assertEqual(rs.move_time, datetime.timedelta(4))
