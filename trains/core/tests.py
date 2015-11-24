eb02d1973a2a75c259808b7852175c618dd9dc19
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-11-24 10:48:45 +0300

    core: add some tests

--- trains/core/tests.py
+++ trains/core/tests.py
@@ -23,12 +23,31 @@ class StationModelTests(django.test.TestCase):
 
 
 class DirectionModelTests(django.test.TestCase):
-    pass
+    def test__has_fields(self):
+        d = trains.core.models.Direction(name='D')
+        d.save()
+        d = trains.core.models.Direction.objects.get(pk=d.id)
+        self.assertEqual(d.name, 'D')
 
 
 class RouteModelTests(django.test.TestCase):
-    pass
+    def test__has_fields(self):
+        d = trains.core.models.Direction(name='D')
+        d.save()
+        r = trains.core.models.Route(name='R', direction=d)
+        r.save()
+        r = trains.core.models.Route.objects.get(pk=r.id)
+        self.assertEqual(r.name, 'R')
+        self.assertEqual(r.direction.name, 'D')
 
 
-class DirectionModelTests(django.test.TestCase):
-    pass
+class WeekdayTests(django.test.TestCase):
+    def test__conversion(self):
+        wd = filter(
+            lambda x: x[0] == trains.core.models.Weekday.wednesday,
+            trains.core.models.Weekday.choices
+        )[0][1]
+        self.assertEqual(
+            wd,
+            trains.core.models.Weekday(trains.core.models.Weekday.wednesday)
+        )
