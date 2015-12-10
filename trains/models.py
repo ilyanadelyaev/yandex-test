798429c52c3346c76635b091268c3e7c825031e8
ilyanadelyaev <nadelyaev.ilya@gmail.com>
2015-12-10 18:54:24 +0300

    docstring to models

--- trains/models.py
+++ trains/models.py
@@ -4,12 +4,15 @@ import trains.tools
 
 
 class Station(django.db.models.Model):
+    """
+    Station abstraction
+    May be extended with extra info
+    """
     name = django.db.models.CharField(max_length=30)
 
     def __unicode__(self):
         return '{} [{}]'.format(self.name, self.id)
 
-    # TODO: drop me
     def directionstation_list(self):
         return self.directionstation_set.all()
 
@@ -18,6 +21,10 @@ class Station(django.db.models.Model):
 
 
 class Direction(django.db.models.Model):
+    """
+    Direction abstraction
+    Store stations in sub-table DirectionStation
+    """
     name = django.db.models.CharField(max_length=60)
 
     def __unicode__(self):
@@ -31,12 +38,23 @@ class Direction(django.db.models.Model):
 
 
 class DirectionStation(django.db.models.Model):
+    """
+    Station position on current direction
+    Started with 0
+    """
     direction = django.db.models.ForeignKey(Direction)
     station = django.db.models.ForeignKey(Station)
     position = django.db.models.PositiveSmallIntegerField()
 
+    def __unicode__(self):
+        return '{} - {} - {} [{}]'.format(
+            self.direction, self.station, self.position, self.id)
+
 
 class Route(django.db.models.Model):
+    """
+    Route on direction with start and end stations
+    """
     name = django.db.models.CharField(max_length=60)
     direction = django.db.models.ForeignKey(Direction)
     start_station = django.db.models.ForeignKey(
@@ -55,6 +73,11 @@ class Route(django.db.models.Model):
 
 
 class RouteStation(django.db.models.Model):
+    """
+    Stations on route with wait time and move time till next station
+    First and last stations have 0 wait time
+    Last station have 0 move time
+    """
     route = django.db.models.ForeignKey(Route)
     station = django.db.models.ForeignKey(Station)
     wait_time = django.db.models.DurationField()
@@ -66,6 +89,9 @@ class RouteStation(django.db.models.Model):
 
 
 class Timetable(django.db.models.Model):
+    """
+    Route week day and time of start
+    """
     route = django.db.models.ForeignKey(Route)
     weekday = django.db.models.PositiveSmallIntegerField(
         choices=trains.tools.Weekday.choices)
